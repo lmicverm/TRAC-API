8bebc420b7c6a25eb8f95a2a9b1fd8c2d47da861
Lennart Vermeir <vermeirl@delawareconsulting.com>
2021-08-20 15:52:55 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request, Response
+import pandas as pd
+
+app = Flask(__name__)
+
+data = {'Name': ['Tom', 'Joseph', 'Krish', 'John'], 'Age': [20, 21, 19, 18]}  
+df = pd.read_csv("./data.csv", delimiter=",") 
+
+
+@app.route("/")
+def hello():
+    return "Hello, this is a simple API app for the delaware TRAC!"
+
+@app.route("/api/productSalesDetails", methods=["POST"])
+def get_product_sales_details():
+    print(">>> get_product_sales_details got a request")
+    post_data = request.get_json()
+    product_id = post_data.get("product_id", None)
+    if product_id is not None:
+        print(f"Product ID = {product_id}")
+        return Response(str(df[df["product_id"]==int(product_id)].to_json(orient="records")), status=200, mimetype="application/json")
+    else:
+        print("Nonetype as product_id")
+        return Response("No product_id was given in the request", status=403, mimetype="application/json")
